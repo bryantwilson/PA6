e7eafa74418ac959256626288b4427a37613723f
Bryant Wilson <bwilson4bama@gmail.com>
2020-12-11 01:58:20 -0600

    Add project files.

---
+++ PA6/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace PA6
+{
+    partial class frmCWID
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCWID = new System.Windows.Forms.TextBox();
+            this.lblCWID = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCWID
+            // 
+            this.txtCWID.Location = new System.Drawing.Point(261, 35);
+            this.txtCWID.Name = "txtCWID";
+            this.txtCWID.Size = new System.Drawing.Size(176, 29);
+            this.txtCWID.TabIndex = 0;
+            // 
+            // lblCWID
+            // 
+            this.lblCWID.AutoSize = true;
+            this.lblCWID.Location = new System.Drawing.Point(57, 35);
+            this.lblCWID.Name = "lblCWID";
+            this.lblCWID.Size = new System.Drawing.Size(158, 24);
+            this.lblCWID.TabIndex = 1;
+            this.lblCWID.Text = "Enter Your CWID:";
+            this.lblCWID.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(261, 93);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 34);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(362, 93);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 34);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmCWID
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 179);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lblCWID);
+            this.Controls.Add(this.txtCWID);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "frmCWID";
+            this.Text = "CWID";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCWID;
+        private System.Windows.Forms.Label lblCWID;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
